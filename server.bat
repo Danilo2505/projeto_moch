29c3552aa946e449e147406d2e2570aa86898a53
Danilo2505 <77727610+Danilo2505@users.noreply.github.com>
2025-11-19 09:06:02 -0300

    Site Institucional

---
+++ server.bat
@@ -0,0 +1,16 @@
+:: python -m venv venv
+@echo off
+REM Change to the directory containing your virtual environment's Scripts folder
+cd venv\Scripts
+
+REM Activate the virtual environment
+call activate.bat
+
+REM Change to your Django project directory
+cd ../../
+
+REM Run the Django development server
+python manage.py runserver
+
+REM Keep the command prompt open after the server starts (optional)
+pause
